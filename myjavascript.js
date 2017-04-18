138d5bee5199d1c1fd49f028017368d1bc209eaf
Oumastrooi <e_g172@txstate.edu>
2017-04-18 16:27:55 -0500

    Fixed the image overlapping

--- myjavascript.js
+++ myjavascript.js
@@ -1,62 +1,64 @@
 // external js: isotope.pkgd.js
 
-// init Isotope
-var $grid = $('.grid').isotope({
-    itemSelector: '.student'
-});
+var $grid = $('.grid').imagesLoaded(function() {
+    // init Isotope after all images have loaded
+    $grid.isotope({
+        itemSelector: '.student',
+        percentPosition: true,
+        masonry: {
+            // use element for option
+            columnWidth: '.grid-sizer'
+        }
+    });
 
-// store filter for each group
-var filters = [];
-var numFilters = 0;
-var iso = $grid.data('isotope');
-var $filterCount = $('.filter-count');
+    // store filter for each group
+    var filters = [];
+    var numFilters = 0;
+    var iso = $grid.data('isotope');
+    var $filterCount = $('.filter-count');
 
-// change is-checked class on buttons
-$('.filters').on('click', 'button', function(event) {
-    var $target = $(event.currentTarget);
-    $target.toggleClass('is-checked');
-    var isChecked = $target.hasClass('is-checked');
-    var filter = $target.attr('data-filter');
-    if (isChecked && numFilters < 3) {
-        addFilter(filter);
-    } else if (isChecked && numFilters >= 3) {
+    // change is-checked class on buttons
+    $('.filters').on('click', 'button', function(event) {
+        var $target = $(event.currentTarget);
         $target.toggleClass('is-checked');
-    } else {
-        removeFilter(filter);
+        var isChecked = $target.hasClass('is-checked');
+        var filter = $target.attr('data-filter');
+        if (isChecked && numFilters < 3) {
+            addFilter(filter);
+        } else if (isChecked && numFilters >= 3) {
+            $target.toggleClass('is-checked');
+        } else {
+            removeFilter(filter);
+        }
+        // filter isotope
+        // group filters together, inclusive
+        $grid.isotope({ filter: filters.join(',') });
+        updateFilterCount();
+
+    });
+
+    function updateFilterCount() {
+        $filterCount.text('Showing ' + iso.filteredItems.length + ' of 48 Designers');
     }
-    // filter isotope
-    // group filters together, inclusive
-    $grid.isotope({ filter: filters.join(',') });
+
     updateFilterCount();
 
-});
-
-function updateFilterCount() {
-    $filterCount.text('Showing ' + iso.filteredItems.length + ' of 48 Designers');
-}
-
-updateFilterCount();
-
-
-
-function addFilter(filter) {
-    if (numFilters < 3) {
-        if (filters.indexOf(filter) == -1) {
-            filters.push(filter);
-            numFilters = numFilters + 1;
+    function addFilter(filter) {
+        if (numFilters < 3) {
+            if (filters.indexOf(filter) == -1) {
+                filters.push(filter);
+                numFilters = numFilters + 1;
+            }
         }
     }
-}
 
-function removeFilter(filter) {
-    var index = filters.indexOf(filter);
-    if (index != -1) {
-        filters.splice(index, 1);
-        numFilters = numFilters - 1;
+    function removeFilter(filter) {
+        var index = filters.indexOf(filter);
+        if (index != -1) {
+            filters.splice(index, 1);
+            numFilters = numFilters - 1;
+        }
     }
-}
 
-$(document).ready(function() {
-    var PageLoadFilter = '.initially-hidden';
-    $container.isotope({ filter: PageLoadFilter });
+
 });
